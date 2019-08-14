11db80247f848dab38e1c90ea2b1c0469a399b5b
Felak <31861697+Felak@users.noreply.github.com>
2019-08-14 09:38:21 +0200

    layouts

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,34 @@
+<footer>
+<div id='footer'>
+    <ul class='footList'>
+        <li><a href="http://" target="_blank" rel="noopener noreferrer">test</a></li>
+        <li><a href="http://" target="_blank" rel="noopener noreferrer">test</a></li>
+        <li><a href="http://" target="_blank" rel="noopener noreferrer">test</a></li>
+        <li><a href="http://" target="_blank" rel="noopener noreferrer">test</a></li>
+        <li><a href="http://" target="_blank" rel="noopener noreferrer">test</a></li>
+        <li><a href="http://" target="_blank" rel="noopener noreferrer">test</a></li>
+        <li><a href="http://" target="_blank" rel="noopener noreferrer">test</a></li>
+        <li><a href="http://" target="_blank" rel="noopener noreferrer">test</a></li>
+    </ul>
+    <ul class='footList'>
+        <li><a href="http://" target="_blank" rel="noopener noreferrer">test</a></li>
+        <li><a href="http://" target="_blank" rel="noopener noreferrer">test</a></li>
+        <li><a href="http://" target="_blank" rel="noopener noreferrer">test</a></li>
+        <li><a href="http://" target="_blank" rel="noopener noreferrer">test</a></li>
+        <li><a href="http://" target="_blank" rel="noopener noreferrer">test</a></li>
+        <li><a href="http://" target="_blank" rel="noopener noreferrer">test</a></li>
+        <li><a href="http://" target="_blank" rel="noopener noreferrer">test</a></li>
+        <li><a href="http://" target="_blank" rel="noopener noreferrer">test</a></li>
+    </ul>
+    <ul class='footList'>
+        <li><a href="http://" target="_blank" rel="noopener noreferrer">test</a></li>
+        <li><a href="http://" target="_blank" rel="noopener noreferrer">test</a></li>
+        <li><a href="http://" target="_blank" rel="noopener noreferrer">test</a></li>
+        <li><a href="http://" target="_blank" rel="noopener noreferrer">test</a></li>
+        <li><a href="http://" target="_blank" rel="noopener noreferrer">test</a></li>
+        <li><a href="http://" target="_blank" rel="noopener noreferrer">test</a></li>
+        <li><a href="http://" target="_blank" rel="noopener noreferrer">test</a></li>
+        <li><a href="http://" target="_blank" rel="noopener noreferrer">test</a></li>
+    </ul >
+    </div>
+</footer>
