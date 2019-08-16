6848dd9411ecca02f071bea841bd9d1c74e6e2a0
aritz11 <sonntag.alexis@gmail.com>
2019-08-16 15:21:59 +0200

    update1

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,24 +1,18 @@
 
     <header>
+        
         <div id="app">
+
+        
+
             <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
                 <div class="container">
                     <a class="navbar-brand" href="{{ url('/') }}">
                         <!--{{ config('app.name', 'Laravel') }}-->
                         MTG-SLAG
                     </a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <!-- Left Side Of Navbar -->
-                        <ul class="navbar-nav mr-auto">
-
-                        </ul>
-
                         <!-- Right Side Of Navbar -->
-                        <ul class="navbar-nav ml-auto">
+                        <ul class="container_log">
                             <!-- Authentication Links -->
                             @guest
                             <li class="nav-item">
@@ -34,9 +28,7 @@
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                    <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
@@ -44,18 +36,18 @@
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                         @csrf
                                     </form>
-                                </div>
+                                
                             </li>
                             @endguest
                         </ul>
-                    </div>
+                        
+                    
                 </div>
             </nav>
-            <div id='middleHeader'>
-    <img  id='logo' src="{{ URL::asset('img/logo.png') }}" alt="" >
-            </div>
+
+
             <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-                <img id='navI' src="{{URL::asset('img/logoLight.png')}}" >
+                <img id='navI' src="{{URL::asset('img/slagwhite.png')}}" >
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -80,4 +72,8 @@
                     </ul>
                 </div>
             </nav>
+            <div id='middleHeader'>
+    <img  id='logo' src="{{ URL::asset('img/slagred.png') }}" alt="" >
+            </div>
+            
     </header>
