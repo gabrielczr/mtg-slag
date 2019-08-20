255cb8ff795b73bcc9ca6868b853a24d4ca29cc7
aritz11 <sonntag.alexis@gmail.com>
2019-08-20 14:55:35 +0200

    layout
    general layout

--- public/js/app.js
+++ public/js/app.js
@@ -49687,16 +49687,28 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
+/***/ "./resources/sass/news.scss":
+/*!**********************************!*\
+  !*** ./resources/sass/news.scss ***!
+  \**********************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
 /***/ 0:
-/*!*******************************************************************************************!*\
-  !*** multi ./resources/js/app.js ./resources/sass/app.scss ./resources/sass/general.scss ***!
-  \*******************************************************************************************/
+/*!**********************************************************************************************************************!*\
+  !*** multi ./resources/js/app.js ./resources/sass/app.scss ./resources/sass/general.scss ./resources/sass/news.scss ***!
+  \**********************************************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\wamp64\www\Magic Final Dev\resources\js\app.js */"./resources/js/app.js");
-__webpack_require__(/*! C:\wamp64\www\Magic Final Dev\resources\sass\app.scss */"./resources/sass/app.scss");
-module.exports = __webpack_require__(/*! C:\wamp64\www\Magic Final Dev\resources\sass\general.scss */"./resources/sass/general.scss");
+__webpack_require__(/*! C:\wamp64\www\alexisT\resources\js\app.js */"./resources/js/app.js");
+__webpack_require__(/*! C:\wamp64\www\alexisT\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\wamp64\www\alexisT\resources\sass\general.scss */"./resources/sass/general.scss");
+module.exports = __webpack_require__(/*! C:\wamp64\www\alexisT\resources\sass\news.scss */"./resources/sass/news.scss");
 
 
 /***/ })
